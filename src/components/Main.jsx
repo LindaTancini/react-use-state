24a1160bb60a341783b1170fec576cf01b0e0073
LindaTancini <lindatancini95@gmail.com>
2025-04-08 14:24:09 +0200

    feat: create and export main with bootstrap

---
+++ src/components/Main.jsx
@@ -0,0 +1,23 @@
+// CREO FUNZIONE DEL COMPONENTE MAIN
+function Main() {
+  return (
+    <main>
+      <div className="container py-5">
+        <div className="d-flex flex-wrap gap-2">
+          <button className="btn btn-primary mb-4">HTML</button>
+          <button className="btn btn-primary mb-4">CSS</button>
+          <button className="btn btn-primary mb-4">JavaScript</button>
+          <div className="card p-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ullam
+            officia autem quam, modi expedita consequatur, assumenda veritatis
+            quidem non totam error, dolore necessitatibus omnis nostrum id culpa
+            sequi perspiciatis.
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+// ESPORTO FUNZIONE MAIN
+export default Main;
