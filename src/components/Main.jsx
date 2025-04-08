152eb15638e835decefd31a101653cb9a795023f
LindaTancini <lindatancini95@gmail.com>
2025-04-08 15:34:04 +0200

    feat: added map, onClick and find

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,19 +6,36 @@ console.log(languages);
 // CREO FUNZIONE DEL COMPONENTE MAIN
 function Main() {
   //CREO VARIABILE REATTIVA
-  const [selectedLanguage, setSelectedLanguage] = useState(0);
+  const [selectedLanguage, setSelectedLanguage] = useState(1);
   return (
     <main>
       <div className="container py-5">
         <div className="d-flex flex-wrap gap-2">
-          <button className="btn btn-primary mb-4">HTML</button>
-          <button className="btn btn-primary mb-4">CSS</button>
-          <button className="btn btn-primary mb-4">JavaScript</button>
+          {/*CON MAP VADO A ITINERARE NELL'ARRAY E PRENDO IL LORO ID E TITLE*/}
+          {languages.map((language) => (
+            <button
+              key={language.id}
+              className="btn btn-primary mb-4"
+              onClick={() => setSelectedLanguage(language.id)}
+            >
+              {language.title}
+            </button>
+          ))}
+
           <div className="card p-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ullam
-            officia autem quam, modi expedita consequatur, assumenda veritatis
-            quidem non totam error, dolore necessitatibus omnis nostrum id culpa
-            sequi perspiciatis.
+            {/* USO FIND PER CERCARE UN DETERMINATO ELEMENTO NELL'ARRAY */}
+            <h4>
+              {
+                languages.find((language) => language.id == selectedLanguage)
+                  .title
+              }
+            </h4>
+            <p>
+              {
+                languages.find((language) => language.id == selectedLanguage)
+                  .description
+              }
+            </p>
           </div>
         </div>
       </div>
