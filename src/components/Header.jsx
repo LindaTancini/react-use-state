5b5b0986031a62e182c027b28db6c6788095ef0e
LindaTancini <lindatancini95@gmail.com>
2025-04-08 13:59:23 +0200

    feat: create and export header

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+// CREO FUNZIONE DEL COMPONENTE HEADER
+function Header() {
+  return (
+    <header>
+      <h1>Learn Web Development</h1>
+    </header>
+  );
+}
+
+// ESPORTO FUNZIONE HEADER
+export default Header;
